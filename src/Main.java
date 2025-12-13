817d0ba39bed51a71f6ab8948fd3c3c7a6a762dd
Lenovo <samsaevaajnur196@gmail.com>
2025-12-13 09:03:44 +0500

    init commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Product apple = new Product();
+        apple.name="Red juicy apple";
+        apple.price=100;
+        apple.quantity=5;
+        apple.barcode=54352412;
+        apple.brand="Sultanovskie";
+    }
+
+}
---
+++ src/Product.java
@@ -0,0 +1,6 @@
+public class Product {
+    public String name;
+    public int price,quantity;
+    public int barcode;
+    public String brand;
+}
